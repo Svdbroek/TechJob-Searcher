fea8e4238fd125c81abd1378cb5fbd7e6c68f5aa
Svdbroek <s_in_the_water@hotmail.com>
2019-11-18 17:58:57 +0100

    feed now builds blocks from data

--- src/Sections/Feed/Feed.js
+++ src/Sections/Feed/Feed.js
@@ -2,12 +2,63 @@ import React, { Component } from "react";
 import JobAplication from "./JobAplication"
 
 export default class Feed extends Component {
-  data = [{name: "jopb 1"},
-                {name: "job2"}]
+  data = [
+    {
+      id: "f08320dd-ed1a-4752-b09d-6172db1594ca",
+      type: "Contract",
+      url:
+        "https://jobs.github.com/positions/f08320dd-ed1a-4752-b09d-6172db1594ca",
+      created_at: "Fri Nov 15 20:38:32 UTC 2019",
+      company: "Hoefler\u0026Co.",
+      company_url: "https://www.typography.com",
+      location: "NYC (remote friendly)",
+      title: "Contract Software Developer\u2028 for Web Applications",
+      description:
+        "\u003cp\u003eH\u0026amp;Co, the New York company that designs and publishes fonts, is looking for a smart, ambitious, and detail-oriented front-end developer who loves typography, and is ready to flex their muscles. Come and join us building the next generation of typography.com, and the award-winning discover.typography.com.\u003c/p\u003e\n\u003cp\u003eOur web applications serve a rare and refreshing audience: tech-savvy designers, the ones who are always running the latest nightly builds on the fastest hardware, but are the first to notice if animation gets triggered on window resize.\u003c/p\u003e\n\u003cp\u003eWe’re looking for a developer who’s up to the challenge of delivering intuitive, performant, and thoughtful experiences to users who know the difference, and have come to expect the best. If you’re looking for an opportunity to do all the things you wish you could at your current job, to join a team of people who share your enthusiasm for what’s possible, and to work for a company that values invention, care, and getting it right, read on.\u003c/p\u003e\n\u003cp\u003eIt goes without saying that we’re looking for candidates with impeccable chops: strong UX instincts, a sharp eye for detail, a mastery of current web technologies and practices, and an unshakable understanding of how the servers come together to deliver the site. You must be fluent in JavaScript using ReactJS and CSS3/HTML5, be familiar with a stylesheet language such as SASS, and live and breathe web standards and optimization. Come in with a list of technologies you’re excited to explore.\nHands-on experience with digital typography is desirable, but not required.\nThis is a contract position, available for either remote work, or working on-site at our New York office, or a combination of the two. The position is open to US citizens or those with authorization to work in the US.\u003c/p\u003e\n\u003cp\u003e\u003cstrong\u003eKey Responsibilities\u003c/strong\u003e\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eWork with our creative team to develop, implement, refine, and deploy new features and functionality across our web properties (typography.com, discover.typography.com, and try.typography.com)\u003c/li\u003e\n\u003cli\u003eWork with our technology and product management team to review project requirements, define criteria for testing, and work with technical management to assess project feasibility within the allotted timetable and budget\u003c/li\u003e\n\u003cli\u003eDevelop and manage a web toolchain including build tools, preprocessors, and open- source libraries. You should be familiar with software such as React, Redux, Yarn, ES6, webpack, PostCSS, and other modern front-end development tools and methodologies\u003c/li\u003e\n\u003cli\u003eUnderstand user needs and behavior by utilizing site analytics, user testing, and data modeling, to refine and improve both web applications and back-office utilities\u003c/li\u003e\n\u003cli\u003eImplement refinements to existing services and create new ones, in order to address changes in standards, customer expectations, behavior patterns, and best practices\u003c/li\u003e\n\u003cli\u003eCoordinate version deployment within a quality assurance framework, using standards and processes that ensure that projects meet defined conditions and specifications\u003c/li\u003e\n\u003cli\u003eMaintain up-to-date knowledge of standards, emerging technologies, industry trends, and best practices\u003c/li\u003e\n\u003c/ul\u003e\n\u003cp\u003e\u003cstrong\u003eRequired Qualifications\u003c/strong\u003e\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eA Bachelor's Degree in a technical or design field, or equivalent experience\u003c/li\u003e\n\u003cli\u003eIndustry experience developing with current versions of HTML, CSS\u003c/li\u003e\n\u003cli\u003eStrong command and professional experience in JavaScript, ES6, and React implemented with RESTful APIs\u003c/li\u003e\n\u003cli\u003eStrong command of web standards, responsive and adaptive design, progressive enhancement and graceful degradation, optimization techniques, dynamic content retrieval, state management, and techniques for accessibility and device-portability\u003c/li\u003e\n\u003cli\u003eExperience writing maintainable code with thorough test coverage, using git SCM and git flow\u003c/li\u003e\n\u003cli\u003eWork experience with Linux/Apache/MySQL/PHP development\u003c/li\u003e\n\u003cli\u003eExperience with CMS theming, multiple frameworks, and databases\u003c/li\u003e\n\u003cli\u003eWork experience with Amazon Web Services (AWS)\u003c/li\u003e\n\u003c/ul\u003e\n",
+      how_to_apply:
+        '\u003cp\u003ePlease send a letter and résumé to Jimmy Mooney at \u003ca href="mailto:mooney@typography.com"\u003emooney@typography.com\u003c/a\u003e. You’re welcome to include any other material that you feel distinguishes you as a candidate. Candidates only, no recruiters or third parties, please.\u003c/p\u003e\n',
+      company_logo:
+        "https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcTEzIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--175e6db44aae3f5ffea51cd5e7a7c18588983d1f/icon-384x384.png"
+    },
+    {
+      id: "95ca1ef3-1140-4b86-b08a-855b1f612ae4",
+      type: "Full Time",
+      url:
+        "https://jobs.github.com/positions/95ca1ef3-1140-4b86-b08a-855b1f612ae4",
+      created_at: "Fri Nov 15 19:02:11 UTC 2019",
+      company: "Yellowdig",
+      company_url: "https://yellowdig.com/",
+      location: "Philadelphia",
+      title: "Frontend Developer",
+      description:
+        "\u003cp\u003eYellowdig is looking for a Frontend Developer to join the team and take on aspects of our user interface and customer experience. We're looking for a candidate who has a strong background with modern javascript and a keen sense of design and style.\u003c/p\u003e\n\u003cp\u003eWe are a dedicated team committed to solving some of the most challenging problems in education. How can today’s educators keep tomorrow’s students engaged and motivated? Our proven social learning platform has demonstrated higher engagement and learning outcome through collaboration, content curation and gamification.\u003c/p\u003e\n\u003cp\u003eYou will work with our rapidly growing customer base to translate their needs into well designed features and functionality.\u003c/p\u003e\n\u003cp\u003eYou:\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eare a self-starter, independent and resourceful\u003c/li\u003e\n\u003cli\u003ehave shipped frontend code to end users\u003c/li\u003e\n\u003cli\u003ework well on in a startup environment\u003c/li\u003e\n\u003cli\u003eare interested in improving education\u003c/li\u003e\n\u003cli\u003eare flexible in sharing responsibility and wearing different hats\u003c/li\u003e\n\u003c/ul\u003e\n\u003cp\u003eTechnologies:\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eES6 Javascript\u003c/li\u003e\n\u003cli\u003eReact\u003c/li\u003e\n\u003cli\u003eRedux\u003c/li\u003e\n\u003cli\u003eCSS/Sass\u003c/li\u003e\n\u003c/ul\u003e\n\u003cp\u003eBonus Points (not required):\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eexperience with functional programming techniques\u003c/li\u003e\n\u003cli\u003eClojure/Clojurescript\u003c/li\u003e\n\u003cli\u003eDatomic\u003c/li\u003e\n\u003c/ul\u003e\n\u003cp\u003eCompensation:\u003c/p\u003e\n\u003cul\u003e\n\u003cli\u003eCompetitive base\u003c/li\u003e\n\u003cli\u003eStock options\u003c/li\u003e\n\u003cli\u003eFully funded health, dental, vision and life insurance\u003c/li\u003e\n\u003cli\u003eOpportunity for rapid career growth\u003c/li\u003e\n\u003c/ul\u003e\n\u003cp\u003eLocation: Philadelphia, USA (Center City)\u003c/p\u003e\n\u003cp\u003eOpportunity to work with exceptionally talented peers in a fast paced start-up environment.\u003c/p\u003e\n",
+      how_to_apply:
+        '\u003cp\u003eEmail your resume to \u003ca href="mailto:brian@yellowdig.com"\u003ebrian@yellowdig.com\u003c/a\u003e\u003c/p\u003e\n\u003cp\u003e\u003ca href="https://github.com/bhurlow"\u003ehttps://github.com/bhurlow\u003c/a\u003e\u003c/p\u003e\n',
+      company_logo:
+        "https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcXAzIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--a81c9f6539c7a4f2375e6e2f958a9715f156a976/yellowdig-logo.png"
+    },
+    {
+      id: "6b01cab3-911e-4d32-b195-bc42bc05e6cd",
+      type: "Part Time",
+      url:
+        "https://jobs.github.com/positions/6b01cab3-911e-4d32-b195-bc42bc05e6cd",
+      created_at: "Fri Nov 15 09:32:08 UTC 2019",
+      company: "Geektastic",
+      company_url: "https://geektastic.com/",
+      location: "Remote",
+      title:
+        "Code Challenge Reviewer - 100% Remote - £50 per hour - Java, JavaScript, PHP, Python, C#, Ruby, CSS/HTML, SQL, Scala, iOS or Android",
+      description:
+        '\u003ch3\u003eFancy earning extra cash reviewing code challenge submissions from any location?\u003c/h3\u003e\n\u003cp\u003eWe pay you \u003cstrong\u003e£25\u003c/strong\u003e for each code challenge you review (30 minutes review time).  You can do as many or as few as you want per week.\u003c/p\u003e\n\u003cp\u003eWe are looking for highly talented Java, JavaScript, PHP, Python, C#, Ruby, Scala, iOS and Android developers\u003c/p\u003e\n\u003cp\u003ePlease read some comments made by our reviewers on \u003ca href="https://www.quora.com/How-reliable-is-Geektastic-Has-anyone-earned-a-reasonable-amount-by-being-a-code-reviewer"\u003eQuora here\u003c/a\u003e \u003c/p\u003e\n\u003cp\u003eWe pay you via Transferwise, Revolut or Payoneer at the end of the month (unless you are in the UK, in which case we bank via bank transfer). \u003c/p\u003e\n\u003cp\u003eTo become part of the team you just need to register with us at \u003ca href="https://geektastic.com/?utm_source=github\u0026amp;utm_medium=jobad\u0026amp;utm_content=jobad\u0026amp;utm_campaign=1012"\u003eGeektastic\u003c/a\u003e and take some code challenges. These are reviewed by our expert team (we need to know how great you are :))\u003c/p\u003e\n\u003cp\u003eOnce you are part of the distributed team you will then be notified on our Slack channel when a new challenge is ready to be reviewed.\u003c/p\u003e\n\u003cp\u003eFeel free to email \u003ca href="mailto:hello@geektastic.com"\u003ehello@geektastic.com\u003c/a\u003e if you have any questions\u003c/p\u003e\n',
+      how_to_apply:
+        '\u003cp\u003ePlease register online @ \u003ca href="https://geektastic.com/?utm_source=github\u0026amp;utm_medium=jobad\u0026amp;utm_content=jobad\u0026amp;utm_campaign=1012"\u003eGeektastic.com\u003c/a\u003e\u003c/p\u003e\n',
+      company_logo:
+        "https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBcVIzIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--11480b0c335acc8d7e24cafba06d1747870cc47e/geektastic-logo_Logo-Square-B-W-02%204500%20pix.png"
+    }]
   
     render() {
     return <div>
-        {this.data.map(jobAdv => <JobAplication name={jobAdv.name}/>) }
+        {this.data.map(jobAdv => <JobAplication title={jobAdv.title} company={jobAdv.company} location={jobAdv.location} description={jobAdv.description} logo={jobAdv.company_logo}/>) }
 }        
     </div>
 }}

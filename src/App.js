cf317c4f4249d9404ffbe7fccb6c6ff4a23215f4
Svdbroek <s_in_the_water@hotmail.com>
2019-11-19 16:01:53 +0100

    api querry is possible, added searching gif

--- src/App.js
+++ src/App.js
@@ -9,38 +9,40 @@ export default class App extends React.Component {
     super();
     this.state = {
       originalData: [],
-      data: []
+      data: [],
+      loading: false
     };
+  }
 
+  Fetcher = (jobTitle, Location) => {
+    this.setState({ loading: true });
     fetch(
       // "https://raw.githubusercontent.com/Svdbroek/TechJob-Searcher/master/src/jobs.json"
-     " https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=1&search=code"
+      `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=${jobTitle}&location=${Location}`
     )
       .then(response => response.json())
       .then(myJson => {
         console.log(myJson);
-        this.setState({ data: myJson, originalData: myJson });
+        this.setState({ data: myJson, loading: false });
       });
-  }
-
-  filter = (jobTitle, region) => {
-    const { originalData } = this.state;
-    const result = originalData.filter(data => {
-      return (
-        data.title.toLowerCase().includes(jobTitle.toLowerCase()) &&
-        data.location.toLowerCase().includes(region.toLowerCase())
-      );
-    });
-    this.setState({ data: result });
   };
 
-  render() {
+  render() { 
     const { data } = this.state;
-
+    if (this.state.loading === true){return<div className="App">
+    <header className="App-header">
+      <Header handleSearch={this.Fetcher} />
+    </header>
+    <main>
+      <img src = "https://static-steelkiwi-dev.s3.amazonaws.com/media/filer_public/4e/07/4e07eece-7c84-46e2-944d-1a6b856d7b5f/463ff844-6f36-4ffe-b051-fea983d39223.gif"/>
+      <Sidebar />
+    </main>
+  </div> }
+    else
     return (
       <div className="App">
         <header className="App-header">
-          <Header handleSearch={this.filter} />
+          <Header handleSearch={this.Fetcher} />
         </header>
         <main>
           <Feed data={data} />

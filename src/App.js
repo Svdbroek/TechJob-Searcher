8b0b5be2ac33727369300b6d1460545d6b622ef5
Svdbroek <s_in_the_water@hotmail.com>
2019-11-19 23:48:42 +0100

    made it so the sidebar loads on top of the feed

--- src/App.js
+++ src/App.js
@@ -61,12 +61,13 @@ export default class App extends React.Component {
           <Header handleSearch={this.Fetcher} />
         </header>
         <main>
+        <Sidebar handleSearch={this.filterByDate}/>
+
           {this.state.loading ? (
             <img src="https://static-steelkiwi-dev.s3.amazonaws.com/media/filer_public/4e/07/4e07eece-7c84-46e2-944d-1a6b856d7b5f/463ff844-6f36-4ffe-b051-fea983d39223.gif" />
           ) : (
             <Feed data={data} />
           )}
-          <Sidebar handleSearch={this.filterByDate}/>
         </main>
       </div>
     );
